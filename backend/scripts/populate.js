08b83e37c6ca1e9a839ccaae31ee336b86080151
priyans619 <priyansh.2b@gmail.com>
2024-06-11 00:37:58 +0530

    function to generate random users

--- backend/scripts/populate.js
+++ backend/scripts/populate.js
@@ -7,4 +7,21 @@ import { mongoURL } from '../config.js';
 // Connect to MongoDB
 mongoose.connect(mongoURL);
 
+// Function to generate random users
+const generateUsers = async (numUsers) => {
+  const users = [];
+  for (let i = 0; i < numUsers; i++) {
+    const user = new User({
+      name: faker.person.fullName(),
+      phoneNumber: faker.phone.number()
+    });
+    users.push(user);
+  }
+  await User.insertMany(users);
+  return users;
+};
+
+
+
+
 populateDatabase();
