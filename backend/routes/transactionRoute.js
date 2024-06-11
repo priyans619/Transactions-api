0d1cb382f8b1b609632d5f7bbf47dbd7921c1353
priyans619 <priyansh.2b@gmail.com>
2024-06-11 20:53:11 +0530

    route for all transaction

--- backend/routes/transactionRoute.js
+++ backend/routes/transactionRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getUserTransactions} from '../controllers/transactionController.js';
+import { getUserTransactions, getAllTransactions} from '../controllers/transactionController.js';
 
 const router = express.Router();
 
 // route for all transactions for a user by user ID
 router.get('/user/:userId', getUserTransactions);
 
+// rouite for all transactions with user details by filters and pagination
+router.get('/', getAllTransactions);
 
 export default router;
 
