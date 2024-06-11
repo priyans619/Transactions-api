42b0e631ebc28abb283a53436c16684d7fedc04f
priyans619 <priyansh.2b@gmail.com>
2024-06-11 17:03:50 +0530

    adding parser and middleware-> userRoute

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRoute from './routes/userRoute.js';
 
 
 const app = express();
@@ -13,6 +14,14 @@ app.get('/', (request, response) => {
   console.log(request)
   return response.status(201).send('Welcome to Transaction-API')
 });
+
+// parsing request
+app.use(express.json());
+
+
+// Middleware for all Routes
+app.use('/users', userRoute);
+
 
 
 mongoose
