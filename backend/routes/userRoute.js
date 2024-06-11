e81a333bac2672246628c993078523acc6bb6789
priyans619 <priyansh.2b@gmail.com>
2024-06-11 17:02:24 +0530

    add route for getting user details by _id

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,15 +1,19 @@
 import express from 'express';
+import { User } from '../models/UserModel.js';
 
 const router = express.Router();
 
-// Get user details by _id
+//  user details by _id
 router.get('/:id', async (req, res) => {
   try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
   }
-    
-  } catch () {
-
-}
 });
 
 export default router;
