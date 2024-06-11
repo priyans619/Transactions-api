aeea522316a0f74d2d08d5b4c364a7387c51fc90
priyans619 <priyansh.2b@gmail.com>
2024-06-11 20:47:35 +0530

    logic for getting all transactions

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -25,4 +25,24 @@ export const getUserTransactions = async (req, res) => {
   }
 };
 
+export const getAllTransactions = async (req, res) => {
+  try {
+    const { status, from, to, type } = req.query;
+    const filters = {};
+
+    if (status) filters.status = status;
+    if (type) filters.type = type;
+    if (from || to) {
+      filters.transactionDate = {};
+      if (from) filters.transactionDate.$gte = new Date(from);
+      if (to) filters.transactionDate.$lte = new Date(to);
+    }
+
+    const transactions = await Transaction.find(filters).populate('userId');
+    res.json(transactions);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
 
