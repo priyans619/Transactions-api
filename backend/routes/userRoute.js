1849d0dd4430e546249ee8c6900b0c3f599dbbe5
priyans619 <priyansh.2b@gmail.com>
2024-06-11 19:51:57 +0530

    route for userdetail by _id

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,20 +1,10 @@
 import express from 'express';
-import { User } from '../models/UserModel.js';
+import { getUserById } from '../controllers/userController.js';
 
 const router = express.Router();
 
-//  user details by _id
-router.get('/:id', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-    res.json(user);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+// Get user details by _id
+router.get('/:id', getUserById);
 
 export default router;
 
