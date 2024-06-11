98581e2db2ecf24eb43e31b66c1c972f924cf8d2
priyans619 <priyansh.2b@gmail.com>
2024-06-11 19:50:49 +0530

    logic for getting user details by _id

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+import { User } from '../models/UserModel.js';
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
