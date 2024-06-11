f1b9ad8790564f7c456f2e8ae923b38d6297851c
priyans619 <priyansh.2b@gmail.com>
2024-06-11 17:00:39 +0530

    userRoute component

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Get user details by _id
+router.get('/:id', async (req, res) => {
+  try {
+  }
+    
+  } catch () {
+
+}
+});
+
+export default router;
+
+
